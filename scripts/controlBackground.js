e3cd273ce6d40065370944f0a8b5b808d2618118
brunex92 <brunex92@gmail.com>
2016-08-04 14:55:06 -0300

    Added commentaries

--- scripts/controlBackground.js
+++ scripts/controlBackground.js
@@ -1,8 +1,12 @@
+////Classe que altera a cor do background
+
 controlBackground = function(scene){
+	//Parâmetros que serao utilizados nos metodos (O colorString começa como a cor padrao do background do Babylon)
 	this.scene = scene;
 	this.colorString = "#33334D"
 };
 
 controlBackground.prototype.updateColor = function() {
+	//Funcao para alterar cor do background = Funcao que transforma cor hexadecimal para padrão RGB
 	this.scene.clearColor = new BABYLON.Color3.FromHexString(this.colorString);
 };
