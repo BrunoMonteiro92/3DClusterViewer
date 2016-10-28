0f5164d51d242b86efa1cda7de5449417b65b841
brunex92 <brunex92@gmail.com>
2016-10-28 15:21:11 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -141,6 +141,9 @@ var initGui = function(axis, grid, cluster, background){
 	folder.add(grid, 'gridOpacity', 0.05, 0.95).name('Grid Opacity').step(0.05).onChange(function(){
 		grid.updateGridOpacity();
 	});
+	folder.add(grid, 'ratio', 0.05, 0.95).name('Grid Ratio').step(1).onChange(function(){
+		grid.updategridRatio();
+	});
 
 	//Cria outra pasta 
 	folder = gui.addFolder('Cluster Options');
