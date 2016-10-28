ec86368e0afe342cd21da1ff23c7ccb61e276f36
brunex92 <brunex92@gmail.com>
2016-10-28 15:22:58 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -141,7 +141,7 @@ var initGui = function(axis, grid, cluster, background){
 	folder.add(grid, 'gridOpacity', 0.05, 0.95).name('Grid Opacity').step(0.05).onChange(function(){
 		grid.updateGridOpacity();
 	});
-	folder.add(grid, 'ratio', 0.05, 0.95).name('Grid Ratio').step(1).onChange(function(){
+	folder.add(grid, 'ratio', 0, 100).name('Grid Ratio').step(1).onChange(function(){
 		grid.updateRatio();
 	});
 
